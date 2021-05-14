aad1bd1ce20cc6b0f73bc9670aa09961280fd63a
Romulo Albanus <romuloaalbanus@hotmail.com>
2021-05-14 21:38:16 +0200

    transactions update

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const cors = require("cors");
 require("./config/db.config")();
 
 const app = express();
-// test 2
-//TEST ROMULO
-//Elvis
+
 app.use(express.json());
 // Não esquecer de criar variável de ambiente com o endereço do seu app React (local ou deployado no Netlify)
 app.use(cors({ origin: process.env.REACT_APP_URL }));
